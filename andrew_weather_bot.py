6162badbbe2fc6314e59cbc0c5f8c263eeb62575
Iaroslav Pavlynskyi <iaroslav.pavlynskyi@globallogic.com>
2019-01-03 16:02:11 +0200

    MODIFY: Weather model is encapsulated in a separate class

--- andrew_weather_bot.py
+++ andrew_weather_bot.py
@@ -5,33 +5,16 @@ from uuid import uuid4
 
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, InlineQueryHandler
-from weather import Weather, Unit
+from model.weatherEntitiy import WeatherEntity
 import telegram
 import os
 import logging
 import currency_rate
 import hryvna_today
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-WEATHER = Weather(unit=Unit.CELSIUS)
-
-
-def get_weather(city):
-    location = WEATHER.lookup_by_location(city)
-    condition = location.condition
-    return """temp: {}, {}""".format(condition.temp, condition.text)
-
-
-def get_forecast(city):
-    location = WEATHER.lookup_by_location(city)
-    forecasts_list = []
-    forecasts = location.forecast
-    for forecast in forecasts[1:6]:
-        forecasts_list.append("""{}
-                Max/Min: {}/{}, {}""".format(forecast.date, forecast.high, forecast.low, forecast.text))
-    return "\n".join(forecasts_list)
 
 
 def start(bot, update):
@@ -41,15 +24,19 @@ def start(bot, update):
 def weather(bot, update, args=[]):
     # set default value
     city = args[0] if bool(args) else 'kharkiv'
+    weather_instanse = WeatherEntity(city)
     logger.info(" Get weather for '{}' city.".format(city))
-    update.message.reply_text("=== {} ==\n {}".format(city,get_weather(city)))
-    # bot.send_photo(chat_id=update.message.chat_id, photo='https://cdn2.iconfinder.com/data/icons/wthr/32/cloudy-512.png')
+    update.message.reply_text("=== {} ==\n {}".format(city, weather_instanse.get_current_weather()))
+    bot.send_photo(chat_id=update.message.chat_id,
+                   photo=weather_instanse.get_weather_image())
 
 
 def forecast(bot, update, args=[]):
     city = args[0] if bool(args) else 'kharkiv'
     logger.info(" Get forecast for '{}' city.".format(city))
-    bot.send_message(chat_id=update.message.chat_id, text="=== {} ==\n {}".format(city, get_forecast(city)),
+    weather_instanse = WeatherEntity(city)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="=== {} ==\n {}".format(city, weather_instanse.get_forecast()),
                      parse_mode=telegram.ParseMode.HTML)
 
 
@@ -67,22 +54,24 @@ def hryvna(bot, update, args=[]):
 
 
 def unknown(bot, update):
-    logger.warning("Unknown command is entered: '{}' by '{}'.".format(update.message['text'], update.message['chat']['username']))
+    logger.warning(
+        "Unknown command is entered: '{}' by '{}'.".format(update.message['text'], update.message['chat']['username']))
     update.message.reply_text("Sorry, I don't know that command.")
 
 
 def inlinequery(bot, update):
+    weather_instanse = WeatherEntity('kharkiv')
     results = [
         InlineQueryResultArticle(
             id=uuid4(),
             title="share_weather",
             input_message_content=InputTextMessageContent(
-                get_weather('kharkiv'))),
+                weather_instanse.get_current_weather())),
         InlineQueryResultArticle(
             id=uuid4(),
             title="share_forecast",
             input_message_content=InputTextMessageContent(
-                get_forecast('kharkiv')))]
+                weather_instanse.get_current_weather()))]
 
     update.inline_query.answer(results)
 
